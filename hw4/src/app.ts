e30e7c6cef8acebf74b953ff00e19805b749c959
Liudmyla <lyudmula1989@ukr.net>
2022-02-22 20:49:16 +0200

    Add hw4

---
+++ hw4/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const a = 22;
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+
+console.log(a);
+app.listen(5501, () => {
+    console.log('Server has started!!!');
+});
